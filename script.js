24d62c03efd1ae76bc0d3e5c3458de4ba22bebbd
LasyaMoravineni <124281083+LasyaMoravineni@users.noreply.github.com>
2023-09-22 17:49:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 
-    //variabkes
+    //variables
     var taskInput = document.getElementById('task-input');
     var taskList = document.getElementById('task-list');
     
 
+    
     /*linking enter key to add button */
     taskInput.addEventListener("keyup",function(event){
       if(event.keyCode==13){
@@ -12,12 +13,14 @@
       }
     });
 
+
     /*function to add item to list */
-    function addTask(){
+    function addTask(taskText){
       
-      const task=taskInput.value;
+      const task = taskText || taskInput.value;
       
-        if (task === '') {
+        if (task === '') 
+        {
             alert('Please enter a task');
             return;
           }
@@ -38,6 +41,7 @@
 
             const newItem= document.createElement("input");
             newItem.classList.add("text");
+            newItem.id="itemname";
             newItem.type="text";
             newItem.value=task;
             newItem.style.cursor="pointer";
@@ -68,7 +72,8 @@
             taskList.appendChild(li);
 
 
-            taskInput.value="";    
+            taskInput.value="";   
+            
 
 
             edit.addEventListener("click",function(e){
@@ -93,20 +98,63 @@
             del.addEventListener("click",function(){
               li.remove();
             })
+            
+            newItem.dataset.completed = 'false'; // Default is false
+
+
             newItem.addEventListener("click",function(){
               if(aActive){
 
-                newItem.style.textDecoration="line-through";
-                newItem.style.color="grey";
+                var isCompleted = newItem.dataset.completed === 'true';
+
+                if (!isCompleted) {
+                  newItem.style.textDecoration = "line-through";
+                  newItem.style.color = "grey";
+                } else {
+                  newItem.style.textDecoration = "none";
+                  newItem.style.color = "black";
+                }
+
+                newItem.dataset.completed = !isCompleted; // Toggle the completed status
+
+                
                 saveData();
               }
-              })
+              });
 
           saveData();
 
             
           }
     }
+
+    function saveData() {
+      var tasks = Array.from(taskList.getElementsByClassName('text')).map(function(task){
+        return {
+          text: task.value,
+          completed: task.dataset.completed === 'true'
+        };
+      });
+    
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+
+    // Load tasks from local storage if available
+    document.addEventListener('DOMContentLoaded', function() {
+      var tasks = JSON.parse(localStorage.getItem('tasks'));
+      if (tasks) {
+          tasks.forEach(function(task) {
+            addTask(task.text);
+            var lastTask = taskList.lastElementChild.getElementsByClassName('text')[0];
+            if (task.completed) {
+              lastTask.style.textDecoration = "line-through";
+              lastTask.style.color = "grey";
+              lastTask.dataset.completed = 'true';
+
+            }
+          });
+      }
+    });
 
     
 
